b8be3f948af410456a26970e8c4911883078c8c6
sofy2831 <s.dumas974@orange.fr>
2026-06-03 10:45:56 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3-8Un0FgBDrvikywmoTJFd5wIuRIyKhk",
+  authDomain: "axe-compta.firebaseapp.com",
+  projectId: "axe-compta",
+  storageBucket: "axe-compta.firebasestorage.app",
+  messagingSenderId: "766523977177",
+  appId: "1:766523977177:web:fa60f502db94ce028b3dc8",
+  measurementId: "G-YTE62FL6V4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
